3f42d2bdc7b4d15fdef9934962630d35e91e7b9f
Agoston Toth <totha014@gmail.com>
2017-12-18 22:09:07 +0100

    Add ajax call function
    The function will make an api call to the freeCodeCamp API
    and processing the response. Than it will temporarly just
    throw the data on the page

--- script.js
+++ script.js
@@ -3,6 +3,25 @@
     https://github.com/DepictYourself
 */
 
+/*
+    This function makes an ajax call to an API
+    we convert the message to a usable json object
+    then display the received data on the webpage
+*/
+function fccApiCall(latitude, longitude) {
+    fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
+    .then( response => response.json())
+    .then( data => {
+        document.getElementById('test').innerHTML = 
+        `
+        lat: ${data.coord.lat}, lon: ${data.coord.lon}
+        ${data.name}, ${data.sys.country}
+        ${data.main.temp}
+        `;
+    })
+    
+}
+
 /* 
     geoSuccess will run when we succesfully received the
     user's location. It will set the latitude and longitude
@@ -11,8 +30,7 @@ function geoSuccess(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    document.getElementById('test').innerHTML = 
-    '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    fccApiCall(latitude, longitude);
 };
 
 function geoError() {
