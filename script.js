515ad4e218141d05436669130622d96e5db6cf47
Agoston Toth <totha014@gmail.com>
2017-12-19 16:14:03 +0100

    Add background image and filter to the weather box

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 /* 
-    written by Agoston Toth
-    https://github.com/DepictYourself
+written by Agoston Toth
+https://github.com/DepictYourself
 */
 
 const main = document.getElementsByTagName("main").item(0);
 
 /*
-    This function makes an ajax call to an API
-    we convert the message to a usable json object
-    then display the received data on the webpage
+This function makes an ajax call to an API
+we convert the message to a usable json object
+then display the received data on the webpage
 */
 function fccApiCall(latitude, longitude) {
     fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
     .then( response => response.json())
-    .then( data => {
-        //TODO Make a proper show results function or module.
-        main.innerHTML = 
-        `
-        <p>${data.name}, ${data.sys.country}</p>
-        <p>${data.main.temp} <span id="degree">°C</span></p>
-        <p>${data.weather[0].main}</p>
-        `;
-    })
+    .then( data => displayData(data))
     
 }
 
 /* 
-    geoSuccess will run when we succesfully received the
-    user's location. It will set the latitude and longitude
+    data argument is the received wether data from the API
+    This function takes care about putting that data on the page
+    and setting the right background image to the main box
+*/
+function displayData(data) {
+    main.innerHTML = 
+    `
+    <p>${data.name}, ${data.sys.country}</p>
+    <p>${data.main.temp} <span id="degree">°C</span></p>
+    <p>${data.weather[0].main}</p>
+    `;
+
+    //TODO changeing background image depending on weather
+}
+
+/* 
+geoSuccess will run when we succesfully received the
+user's location. It will set the latitude and longitude
 */
 function geoSuccess(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-
+    
     fccApiCall(latitude, longitude);
 };
 
@@ -43,14 +51,14 @@ function geoError() {
 /* Checking if the geolocation is supported in the browser */
 if ("geolocation" in navigator) {
     main.innerHTML = "geolocation is available";
-
+    
     /* 
     Trying to get the users position. If everything goes ok 
     the geoSuccess function will run
     If we can't, the geoError function will run.
     */
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-
+    
 } else {
     main.innerHTML = "geolocation IS NOT available";
 }
