f368dfe4c40cb81bcc3f49fbce8997ea871fda33
Agoston Toth <totha014@gmail.com>
2017-12-21 12:59:59 +0100

    Add loading animation

--- script.js
+++ script.js
@@ -75,7 +75,22 @@ function geoError() {
 
 /* Checking if the geolocation is supported in the browser */
 if ("geolocation" in navigator) {
-    main.innerHTML = "<p>Loading</p>";
+    main.innerHTML = 
+    `
+    <!-- loading animation -->
+    <p>Loading</p>
+    <div class="sk-cube-grid">
+    <div class="sk-cube sk-cube1"></div>
+    <div class="sk-cube sk-cube2"></div>
+    <div class="sk-cube sk-cube3"></div>
+    <div class="sk-cube sk-cube4"></div>
+    <div class="sk-cube sk-cube5"></div>
+    <div class="sk-cube sk-cube6"></div>
+    <div class="sk-cube sk-cube7"></div>
+    <div class="sk-cube sk-cube8"></div>
+    <div class="sk-cube sk-cube9"></div>
+    </div>
+    `;
     
     /* 
         Trying to get the users position. If everything goes ok 
