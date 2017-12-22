0fd010d36cade19df4d2c0c79b0e213e544d0e65
Agoston Toth <totha014@gmail.com>
2017-12-22 17:01:05 +0100

    Add feature convert between celsius and fahrenheit

--- script.js
+++ script.js
@@ -14,25 +14,28 @@ const main = document.getElementsByTagName("main").item(0);
 */
 function fccApiCall(latitude, longitude) {
     fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
-    .then( response => response.json())
-    .then( data => displayData(data))
+        .then(response => response.json())
+        .then(data => displayData(data))
 };
 
 /*
-    data argument is the received weather data from the API
+    the data parameter is the received weather data from the API
     This function will put that data on the page
     and set the right background image to the main box
 */
 function displayData(data) {
     let city = data.name;
     let countryCode = data.sys.country;
-    let temp = Math.round(data.main.temp * 10) / 10;
+    // round the temperature to 1 decimal place
+    let tempCelsius = Math.round(data.main.temp * 10) / 10;
+    // convert celsius to fahrenheit
+    let tempFahrenheit = Math.round(data.main.temp * 1.8 + 32);
     let weather = data.weather[0].main;
 
-    main.innerHTML = 
-    `
+    main.innerHTML =
+        `
     <p>${city}, ${countryCode}</p>
-    <p>${temp} <span id="degree">°C</span></p>
+    <p>${tempCelsius}</p><span id="degree">°C</span>
     <p>${weather}</p>
     
     `;
@@ -42,6 +45,24 @@ function displayData(data) {
         to the current weather
     */
     setBackgroundImage(main, `img/${weather.toLowerCase()}.jpg`);
+
+    /*
+        This eventlistener takes care about the temperature
+        conversion from celsius to fahrenheit.
+    */
+    document.getElementById('degree').addEventListener('click', () => {
+        const degreeElement = document.getElementById('degree');
+        const degrees = degreeElement.previousSibling;
+
+        if (degreeElement.innerHTML === "°C") {
+            degrees.innerHTML = tempFahrenheit;
+            degreeElement.innerHTML = "°F";
+        } else {
+            degrees.innerHTML = tempCelsius;
+            degreeElement.innerHTML = "°C";
+        }
+    });
+
 };
 
 /*
@@ -60,13 +81,13 @@ function setBackgroundImage(domElement, imageUrl) {
 function geoSuccess(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    
+
     fccApiCall(latitude, longitude);
 };
 
 function geoError() {
     main.innerHTML =
-    `
+        `
     <p>Error!</p>
     <img width="75px" src="img/sad-face.svg" alt="sad face">
     `
@@ -74,8 +95,8 @@ function geoError() {
 
 /* Checking if the geolocation is supported in the browser */
 if ("geolocation" in navigator) {
-    main.innerHTML = 
-    `
+    main.innerHTML =
+        `
     <!-- loading animation -->
     <p>Waiting</p>
     <div class="sk-cube-grid">
@@ -90,17 +111,17 @@ if ("geolocation" in navigator) {
     <div class="sk-cube sk-cube9"></div>
     </div>
     `;
-    
+
     /* 
         Trying to get the users position. If everything goes ok 
         the geoSuccess function will run
         If something goes wrong the geoError function will run.
     */
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-    
+
 } else {
-    main.innerHTML = 
-    `
+    main.innerHTML =
+        `
     <p>Geolocation not supported</p>
     <img width="75px" src="img/sad-face.svg" alt="sad face">
     `;
